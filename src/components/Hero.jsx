ad02a63017b22b0895652854a55c513f9c5d4aa8
Osama Barakat <104956790+OsamaBarakaat@users.noreply.github.com>
2023-10-08 09:44:53 +0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <section id="" className="bg-white dark:bg-gray-900">
+    <section id="" className="bg-beige dark:bg-gray-900">
       <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="place-self-center justify-center justify-self-center mx-auto lg:col-span-7">
           <h1 className="max-w-2xl mb-4 text-4xl font-bold tracking-tight leading-none md:text-5xl xl:text-6xl text-teal-700 dark:text-white">
